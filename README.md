b6d4586a84f827039365bb2ab1c8bc497b4e7abd
raikutro <raikutro@gmail.com>
2022-12-30 23:57:42 -0800

    Updated descriptions

---
+++ README.md
@@ -0,0 +1,8 @@
+Kusari
+---
+
+> A Markov-chain n-gram Language Model Assisted by a BERT WordPiece Tokenizer by HuggingFace and Word2Vec by Google.
+
+---
+
+Start by running `train_models.js`. Look at the comments inside for details on training.
--- train_models.js
+++ train_models.js
@@ -1,11 +1,11 @@
 let [INPUT_FILE, OUTPUT_W2V, OUTPUT_TOKENS, OUTPUT_TOKEN_MODEL, OUTPUT_KUSARI] = process.argv.slice(2);
 const { trainAll, trainKusari } = require('./process/trainer');
 
-INPUT_FILE = INPUT_FILE || 'data/clean_logs.txt';
-OUTPUT_W2V = OUTPUT_W2V || 'models/w2v_model.txt';
-OUTPUT_TOKENS = OUTPUT_TOKENS || 'data/tknzd.txt';
-OUTPUT_TOKEN_MODEL = OUTPUT_TOKEN_MODEL || 'models/tknzr.json';
-OUTPUT_KUSARI = OUTPUT_KUSARI || 'models/untitled.model';
+INPUT_FILE = INPUT_FILE || 'data/clean_logs.txt'; // Input text file you want to train on. Separate documents by line.
+OUTPUT_W2V = OUTPUT_W2V || 'models/w2v_model.txt'; // Output Word2Vec Model File
+OUTPUT_TOKENS = OUTPUT_TOKENS || 'data/tknzd.txt'; // The destination of the tokenized training file
+OUTPUT_TOKEN_MODEL = OUTPUT_TOKEN_MODEL || 'models/tknzr.json'; // The destination of the BERT Tokenizer model
+OUTPUT_KUSARI = OUTPUT_KUSARI || 'models/untitled.model'; // The destination of the Kusari model
 
 (async () => {
 	let startDate = Date.now();
